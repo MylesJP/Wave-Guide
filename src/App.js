cb91c46c77386d998c383dd1c180a94b222aa982
MylesJP <96148570+MylesJP@users.noreply.github.com>
2023-04-20 14:21:16 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import SearchBar from "./components/SearchBar";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
 
 function App() {
   const [location, setLocation] = useState("");
@@ -7,9 +9,9 @@ function App() {
   const [forecastData, setforecastData] = useState({});
 
   function clickFunctions() {
-    //searchLocation()
-    //getForecast()
-    console.log("Some stuff");
+    //searchLocation() TODO
+    //getForecast() TODO
+    console.log({ location }.location);
   }
 
   const searchLocation = () => {
@@ -27,6 +29,22 @@ function App() {
             clickFunctions={clickFunctions}
           />
         </div>
+        <div className="top">
+          <div className="location">
+            <p>
+              <FontAwesomeIcon icon={faHeart} size="lg" style={{color: "#ffffff",}} />
+              <h1>Tofino, BC</h1>              
+            </p>
+
+          </div>
+          <div className="temp">
+            <p>25°C</p>
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+        {data.main !== undefined && <h2>36 Hours</h2>}
       </div>
     </div>
   );
