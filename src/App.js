0ac0ca3d4d3a8a87170604e3f65e8ccf9ca89b90
MylesJP <96148570+MylesJP@users.noreply.github.com>
2023-04-20 13:41:19 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import SearchBar from "./components/SearchBar";
+import { useState } from "react";
 
 function App() {
+  const [location, setLocation] = useState("");
+  const [data, setData] = useState({});
+  const [forecastData, setforecastData] = useState({});
+
+  function clickFunctions() {
+    //searchLocation()
+    //getForecast()
+    console.log("Some stuff");
+  }
+
+  const searchLocation = () => {
+    // This will be where the microservice passes me information
+    console.log("location goes here");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="search">
+          <SearchBar
+            location={location}
+            setLocation={setLocation}
+            clickFunctions={clickFunctions}
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function SearchBar({ location, setLocation, clickFunctions }) {
+  const shouldDisplayClear = location.length > 0;
+
+  const searchOnEnter = (event) => {
+    if (event.key === "Enter") {
+      clickFunctions();
+    }
+  };
+
+  return (
+    <>
+      <div className="fa fa-search" onClick={clickFunctions}></div>
+      <input
+        value={location}
+        type="text"
+        onKeyDown={searchOnEnter}
+        onChange={(event) => setLocation(event.target.value)}
+        placeholder="Enter Location"
+      />
+      {shouldDisplayClear && (
+        <div className="fa fa-times" onClick={() => setLocation("")}></div>
+      )}
+    </>
+  );
+}
